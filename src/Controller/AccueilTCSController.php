d31b9dd2e57f1dde48339ee61071c9acdae331d5
DarwinAngolaR <darwinangolar@gmail.com>
2021-11-19 11:43:34 +0100

    creation prototipo controller et page twig accueil

---
+++ src/Controller/AccueilTCSController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilTCSController extends AbstractController
+{
+    #[Route('/accueiltcs', name: 'accueil_t_c_s')]
+
+    public function index(): Response
+    {
+        return $this->render('accueil_tcs/index.html.twig', [
+            'controller_name' => 'AccueilTCSController',
+        ]);
+    }
+
+    #[Route('/', name: 'home')]
+    public function home()
+    {
+        return $this->render('accueil_tcs/home.html.twig');
+    }
+}
