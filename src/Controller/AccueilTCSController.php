599f9ba751decb3c9f373cde368621d8ee0d6a06
DarwinAngolaR <darwinangolar@gmail.com>
2021-11-30 13:56:09 +0100

    solution conextion merge Emma

--- src/Controller/AccueilTCSController.php
+++ src/Controller/AccueilTCSController.php
@@ -5,10 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Repository\ArticleRepository;
 
 class AccueilTCSController extends AbstractController
 
 {
+
+    private $repoArticle;
+    private $repoCategory;
+
+    public function __construct(
+        ArticleRepository $repoArticle,
+        CategoryRepository $repoCategory
+    ) {
+        $this->repoArticle = $repoArticle;
+        $this->repoCategory = $repoCategory;
+    }
+
     #[Route('/accueiltcs', name: 'accueil_t_c_s')]
     public function index()
     {
@@ -39,5 +55,48 @@ class AccueilTCSController extends AbstractController
     public function agir_ensemble()
     {
         return $this->render('accueil_tcs/agir_ensemble.html.twig');
+    }
+
+    #[Route('/voyages', name: 'voyages')]
+    public function voyages()
+    {
+        $categories = $this->repoCategory->findAll();
+        $articles = $this->repoArticle->findAll();
+        return $this->render('accueil_tcs/voyages.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name = "show")
+     */
+    public function show(Article $article): Response
+    {
+        if (!$article) {
+            return $this->redirectToRoute('voyages');
+        }
+
+        return $this->render('show/index.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/showArticles/{id}", name = "show_article")
+     */
+    public function showArticle(?Category $category): Response
+    {
+        if ($category) {
+            $articles = $category->getArticles()->getValues();
+        } else {
+            return $this->redirectToRoute('voyages');
+        }
+
+        $categories = $this->repoCategory->findAll();
+        return $this->render('show/showArticle.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories
+        ]);
     }
 }
